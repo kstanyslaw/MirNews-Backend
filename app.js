23012079df3517d2b7054d9159fee910480a0951
Unknown <k.stanyslaw@gmail.com>
2019-02-05 17:30:31 +0700

    Add mongoose
    Add mongoose, DB connection & catch errors

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// connect to MongoDB
+mongoose.connect('mongodb://main_author:main0author@ds048279.mlab.com:48279/mirnews', { useNewUrlParser: true }).then(
+    () => { console.log('\x1b[1m', 'Successfuly connected to DataBase:)\n', '\x1b[0m') },
+    err => {
+        console.error('\x1b[31m', 'Connection to DataBase failed because of:', err.errmsg, '\x1b[0m');
+        console.error('See also full error message: \n', err)
+    }
+);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,18 +34,18 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
