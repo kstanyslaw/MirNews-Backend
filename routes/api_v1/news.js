8dd78adba834a98617a68773ba0583808917d0b7
Stan <k.stanyslaw@gmail.com>
2019-02-07 03:31:31 +0700

    Made get news method
    Made get news method --works
    made post news method to save news do database --don't know works or not

--- routes/api_v1/news.js
+++ routes/api_v1/news.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+const News = require('../../models/news');
+
+// Get All News
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'news' });
-});
+  News.find()
+    .exec(function(err, news) {
+      if(err) {
+        error(500, "An error occured. Can't get any news from DataBase:(", err, res)
+      }
+      res.status(200).json(news)
+    })
+})
+
+// Save one news
+router.post('/', function(req, res, next) {
+  const news = new News({
+    title: req.body.title,
+    author: req.body.author,
+    date: req.body.date,
+    article: req.body.article,
+    categories: req.body.categories,
+  });
+
+  news.save(function(err, result) {
+    if(err) {
+      error(500, "An error occured. Can't save the news to DataBase:(", err, res);
+    }
+    res.status(201).json(result)
+  })
+})
+
+function error(status, message, err, res) {
+  console.error('\x1b[31m', message, '\n', '\x1b[0m', err);
+  return res.status(status).json({
+    title: message,
+    error: err
+  })
+}
 
 module.exports = router;
