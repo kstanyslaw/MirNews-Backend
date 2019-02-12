d42afee2318d70ca017bf5f5811f5ab2fe92ca0b
Stan <k.stanyslaw@gmail.com>
2019-02-12 17:01:34 +0700

    Remade news schema and post method
    New schema contains titles and articles in multiple languages

--- models/news.js
+++ models/news.js
@@ -2,11 +2,18 @@ var mongoose = require('mongoose');
 var mongooseUniqueValidator = require('mongoose-unique-validator');
 var Schema = mongoose.Schema;
 
-var schema = new Schema({
+// Child schema for single language article body
+var bodySchema = new Schema({
+    language: {type: String, required: true},
     title: {type: String, required: true},
+    article: {type: String, required: true},
+}, {_id: false});
+
+// Parent schema for news with array of news in 
+var schema = new Schema({
     author: {type: String, required: true},
     date: {type: Date, required: true},
-    article: {type: String, required: true},
+    body: [bodySchema],
     categories: [{ type: String }],
 })
 
