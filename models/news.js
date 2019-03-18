998e903721e92a216444685bdc54c9ce90cc3c28
Unknown <k.stanyslaw@gmail.com>
2019-03-18 18:26:27 +0700

    Fix news model

--- models/news.js
+++ models/news.js
@@ -4,38 +4,40 @@ var mongoosePaginate = require('mongoose-paginate');
 var Schema = mongoose.Schema;
 
 // Children Schemas
+//      Article Schema
 var articleSchema = new Schema({
-    en: {type: String, required: true},
-    ru: {type: String, required: true},
-}, {_id: false});
-
+    en: { type: String, required: true },
+    ru: { type: String, required: true },
+}, { _id: false });
+//      Title Schema
 var titleSchema = new Schema({
-    en: {type: String, required: true},
-    ru: {type: String, required: true},
-}, {_id: false});
-
+    en: { type: String, required: true },
+    ru: { type: String, required: true },
+}, { _id: false });
+//      Preview Schema
 var previewSchema = new Schema({
-    en: {type: String, required: true},
-    ru: {type: String, required: true},
-}, {_id: false});
-
+    en: { type: String, required: true },
+    ru: { type: String, required: true },
+    img: { type: String, required: true }
+}, { _id: false });
+//      Category Schema
 var categorySchema = new Schema({
-    en: {type: String, required: true},
-    ru: {type: String, required: true},
-    navColor: {type: String, required: true},
-    navbar: {type: Number, required: true},
-    pageColor: {type: String, required: true},
+    en: { type: String, required: true },
+    ru: { type: String, required: true },
+    navColor: { type: String, required: true },
+    navbar: { type: Number, required: true },
+    pageColor: { type: String, required: true },
 })
 
 // Parent Schema
 var schema = new Schema({
-    author: {type: String, required: true},
-    date: {type: Date, required: true},
+    author: { type: String, required: true },
+    date: { type: Date, required: true },
     title: titleSchema,
     article: articleSchema,
     preview: previewSchema,
     category: [categorySchema],
-    img: [{type: String}],
+    img: [{ type: String }],
 })
 
 schema.plugin(mongooseUniqueValidator);
